e9c25541a95d420a5ac69795a13b1c994b454349
holikaradhakrishna-spec <holikaradhakrishna-spec@users.noreply.github.com>
2026-06-17 22:34:13 +0530

    feat: implement task creation form

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -8,3 +8,28 @@ Template.mainContainer.helpers({
     return TasksCollection.find({}, { sort: { createdAt: -1 } });
   },
 });
+
+Template.mainContainer.events({
+  'submit .task-form'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value.trim();
+
+    if (text.length === 0) {
+      return;
+    }
+
+    // Insert a task into the collection
+    TasksCollection.insertAsync({
+      text,
+      createdAt: new Date(), // current time
+      isChecked: false,
+    });
+
+    // Clear form
+    target.text.value = '';
+  },
+});
