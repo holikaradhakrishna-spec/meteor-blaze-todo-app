dc87d6ae7d67fed7e5b2fe53fffc773713797b18
holikaradhakrishna-spec <holikaradhakrishna-spec@users.noreply.github.com>
2026-06-18 13:08:48 +0530

    feat: add task filtering and pending count

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,10 +2,26 @@ import './App.html';
 import './Task.js';
 import { TasksCollection } from '../api/TasksCollection.js';
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
+const state = new ReactiveDict();
+
+Template.mainContainer.onCreated(function() {
+  state.set('hideCompleted', false);
+});
 
 Template.mainContainer.helpers({
   tasks() {
+    if (state.get('hideCompleted')) {
+      return TasksCollection.find({ isChecked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
     return TasksCollection.find({}, { sort: { createdAt: -1 } });
+  },
+  hideCompleted() {
+    return state.get('hideCompleted');
+  },
+  pendingTasksCount() {
+    return TasksCollection.find({ isChecked: { $ne: true } }).count();
   },
 });
 
@@ -31,5 +47,8 @@ Template.mainContainer.events({
 
     // Clear form
     target.text.value = '';
+  },
+  'click .hide-completed'() {
+    state.set('hideCompleted', !state.get('hideCompleted'));
   },
 });
